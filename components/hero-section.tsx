96696eeadc1d1e6a0874ed6803c54bb7e5504981
davidddeveloper <programmingdonebydavid@gmail.com>
2025-11-27 11:58:00 +0000

    made changes to the image

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -11,11 +11,11 @@ export function HeroSection({ onOpenForm }: HeroSectionProps) {
   return (
     <section className="bg-gray-100 py-24 md:py-16 lg:py-16 relative overflow-hidden "> {/*bg-gray-100*/}
       <div className="container mx-auto px-4">
-        <div className="items-center gap-8 grid lg:grid-cols-[300px_1fr_0px]"> {/* grid lg:grid-cols-[300px_1fr_100px] */}
+        <div className="items-center gap-8 grid lg:grid-cols-[250px_1fr_0px]"> {/* grid lg:grid-cols-[300px_1fr_100px] */}
           {/* Left Person Image */}
           <div className="hidden lg:block">
             <img
-              src="/images/joeabasshero.png"
+              src="/images/joeabasshero2.png"
               alt="Team member"
               className="w-full h-auto object-cover"
             />
@@ -26,10 +26,10 @@ export function HeroSection({ onOpenForm }: HeroSectionProps) {
             <h1 className="text-2xl md:text-3xl lg:text-5xl xl:text-5xl font-black text-gray-900 mb-6 leading-tight text-balance">
               ARE YOU READY TO SCALE YOUR BUSINESS?
             </h1>
-            <p className="text-lg md:text-xl lg:text-lg xl:text-2xl text-gray-700 leading-relaxed lg:text-nowrap">
-              Learn from the team that has scaled businesses to $1.5M+ in annual revenue.
+            <p className="text-lg md:text-xl lg:text-lg xl:text-[22px] text-gray-700 leading-relaxed lg:text-nowrap">
+              Learn from the team that has scaled businesses to multimillion US dollars in annual revenue.
             </p>
-            <p className="text-lg md:text-xl lg:text-lg xl:text-2xl text-gray-700 mb-8 leading-relaxed">
+            <p className="text-lg md:text-xl lg:text-lg xl:text-xl text-gray-700 mb-8 leading-relaxed">
               Check whether you qualify for our free business diagnostics.
             </p>
             <Button
