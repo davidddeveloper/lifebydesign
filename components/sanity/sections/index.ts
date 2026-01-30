d5cecde001fcfcb2df6e61076ef56c78db6f5643
David <founder@10na.city>
2026-01-30 17:23:34 +0000

    fix missing section component exports

--- components/sanity/sections/index.ts
+++ components/sanity/sections/index.ts
@@ -1,32 +1,4 @@
-// Home Page Sections
-export { HeroSectionComponent } from './HeroSection'
-export { FAQSectionComponent } from './FAQSection'
-export { FounderSectionComponent } from './FounderSection'
-export { PartnersSectionComponent } from './PartnersSection'
-export { CTASectionComponent } from './CTASection'
-
-// About Page Sections
-export { AboutHeroComponent } from './AboutHero'
-export { AboutStoryComponent } from './AboutStory'
-export { MissionVisionComponent } from './MissionVision'
-export { CoreValuesComponent } from './CoreValues'
-export { ServicesSectionComponent } from './ServicesSection'
-export { ImpactStatsComponent } from './ImpactStats'
-
-// Careers Page Sections
-export { CareersHeroComponent } from './CareersHero'
-export { CareersMindsetComponent } from './CareersMindset'
-export { CareersTestimonialsComponent } from './CareersTestimonials'
-export { CareersValuesComponent } from './CareersValues'
-export { CareersBenefitsComponent } from './CareersBenefits'
-
 // Product Page Sections
-export { ProductHeroComponent } from './ProductHero'
-export { ProcessStepsComponent } from './ProcessSteps'
-export { TargetStagesComponent } from './TargetStages'
-export { PricingPathsComponent } from './PricingPaths'
-export { OutcomesSectionComponent } from './OutcomesSection'
-export { ProductPromiseComponent } from './ProductPromise'
 export { DeliverablesComponent } from './Deliverables'
 export { WorkflowPhasesComponent } from './WorkflowPhases'
 export { PricingPlansComponent } from './PricingPlans'
